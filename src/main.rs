1bbdaaf7e0f751a12d645ca0c8b1d9fed7420c3e
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-07-18 21:27:10 +0900

    Successed to load Lua file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-mod error;
-mod lua;
+use camino::Utf8PathBuf;
+use my_sync::lua::LuaLoader;
 
 fn main() {
-    println!("Hello, world!");
+    let mut target_dir = Utf8PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    target_dir.push("example");
+    let loader = LuaLoader::from_buf(target_dir).expect("Failed to initialization for loading Lua");
+    let result = loader.load("sample.lua");
+    if result.is_ok() {
+        println!("Successed to load Lua file");
+    } else {
+        println!("Failed to load Lua file");
+    }
 }
