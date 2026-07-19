51ae4ddb24aa0969f263407048e3baff6c8b5471
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-07-19 11:21:01 +0900

    added a lua function that returns value

--- src/lua/functions.rs
+++ src/lua/functions.rs
@@ -5,12 +5,20 @@ fn fn_println(_: &Lua, msg: String) -> Result<()> {
     Ok(())
 }
 
+fn fn_add(_: &Lua, (a, b): (u32, u32)) -> Result<u32> {
+    Ok(a + b)
+}
+
 pub fn set_functions(lua: &Lua) -> Result<Table> {
     let module: Table = lua.create_table()?;
 
-    // lua func: print lines
+    // lua function: print lines
     let lua_fn_println = lua.create_function(fn_println)?;
     module.set("println", lua_fn_println)?;
+
+    // lua function: add
+    let lua_fn_add = lua.create_function(fn_add)?;
+    module.set("add", lua_fn_add)?;
 
     Ok(module)
 }
@@ -41,5 +49,28 @@ mod tests {
         let lua = Lua::new();
         let result = fn_println(&lua, "test".to_string());
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_fn_add() {
+        let lua = Lua::new();
+        let result = fn_add(&lua, (1, 1));
+        assert!(result.is_ok());
+        assert!(matches!(result, Ok(2)));
+    }
+
+    #[test]
+    fn test_fn_add_integration() {
+        let lua = Lua::new();
+        inject_package(&lua).unwrap();
+
+        let lua_script = r#"
+            local sync = require("my-sync")
+            return sync.add(1, 2)
+        "#;
+
+        let result: u32 = lua.load(lua_script).eval().unwrap();
+
+        assert_eq!(result, 3);
     }
 }
